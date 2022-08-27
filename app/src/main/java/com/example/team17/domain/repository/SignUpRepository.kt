d6156f14b46b0cf313d46320150c1c0346c719b3
chaeyeong <shimcy2010@gmail.com>
2022-08-27 19:09:14 +0900

    [feat/#4] 회원가입 (닉네임/팀 검색) 뷰

---
+++ app/src/main/java/com/example/team17/domain/repository/SignUpRepository.kt
@@ -0,0 +1,4 @@
+package com.example.team17.domain.repository
+
+class SignUpRepository {
+}
---
+++ app/src/main/java/com/example/team17/presentation/view/user/SignUpActivity.kt
@@ -0,0 +1,28 @@
+package com.example.team17.presentation.view.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.team17.R
+import com.example.team17.databinding.ActivitySignUpBinding
+import com.example.team17.presentation.base.BaseActivity
+
+class SignUpActivity : BaseActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
+    private lateinit var navController: NavController
+    private var signUpNicknameFragment: SignUpNicknameFragment = SignUpNicknameFragment()
+    private var signUpSelectTeamFragment: SignUpSelectTeamFragment = SignUpSelectTeamFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        signUpNicknameFragment = SignUpNicknameFragment()
+        signUpSelectTeamFragment = SignUpSelectTeamFragment()
+        
+    }
+
+
+    override fun onSupportNavigateUp() = findNavController(R.id.nav_signup).navigateUp()
+}
