272892503a001b6247ad6e9b5f91d76af82a406b
chaeyeong <shimcy2010@gmail.com>
2022-08-27 23:40:43 +0900

    [feat/#7] 보글 추가 뷰

---
+++ app/src/main/java/com/example/team17/presentation/view/add/AddVoteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.team17.presentation.view.add
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddVoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_vote)
+    }
+}
