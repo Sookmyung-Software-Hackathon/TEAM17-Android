5ab863d94a8e9b8416b78e6396713c062130c1a4
chaeyeong <shimcy2010@gmail.com>
2022-08-27 21:20:56 +0900

    [feat/#7] 추가 바텀시트

---
+++ app/src/main/java/com/example/team17/presentation/view/add/AddBottomSheetFragment.kt
@@ -0,0 +1,29 @@
+package com.example.team17.presentation.view.add
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.team17.R
+import com.example.team17.databinding.FragmentAddBottomSheetBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class AddBottomSheetFragment : BottomSheetDialogFragment() {
+
+    lateinit var _binding: FragmentAddBottomSheetBinding
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAddBottomSheetBinding.inflate(layoutInflater, container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
