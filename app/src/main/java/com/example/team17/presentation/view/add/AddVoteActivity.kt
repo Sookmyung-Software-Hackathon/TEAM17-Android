c560ac79e2edd3dbcfe54c555ef93188765fb6b2
chaeyeong <shimcy2010@gmail.com>
2022-08-27 23:59:56 +0900

    [feat/#7] 투표 추가 뷰

--- app/src/main/java/com/example/team17/presentation/view/add/AddVoteActivity.kt
+++ app/src/main/java/com/example/team17/presentation/view/add/AddVoteActivity.kt
@@ -1,11 +1,12 @@
 package com.example.team17.presentation.view.add
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.team17.R
+import com.example.team17.databinding.ActivityAddBoggleBinding
+import com.example.team17.presentation.base.BaseActivity
 
-class AddVoteActivity : AppCompatActivity() {
+class AddVoteActivity : BaseActivity<ActivityAddBoggleBinding>(R.layout.activity_add_vote) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_vote)
     }
 }
